aa6b97f0879e334bcd45ef243e7556f06fd897eb
Michael Yuan <michael.yuan@zoom.us>
2019-12-27 17:46:29 +0800

    create custom flow layout
    create custom flow layout

---
+++ CustomCollectionViewFlowLayoutDemo/CustomCollectionViewFlowLayoutDemo/CustomCollectionViewFlowLayout.h
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewFlowLayout.h
+//  CustomCollectionViewFlowLayoutDemo
+//
+//  Created by Admin on 2019/12/27.
+//  Copyright © 2019 com.personal.project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomCollectionViewFlowLayout : UICollectionViewFlowLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CustomCollectionViewFlowLayoutDemo/CustomCollectionViewFlowLayoutDemo/CustomCollectionViewFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewFlowLayout.m
+//  CustomCollectionViewFlowLayoutDemo
+//
+//  Created by Admin on 2019/12/27.
+//  Copyright © 2019 com.personal.project. All rights reserved.
+//
+
+#import "CustomCollectionViewFlowLayout.h"
+
+@implementation CustomCollectionViewFlowLayout
+
+@end
